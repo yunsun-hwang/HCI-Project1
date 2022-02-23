7129013e1bb393db022aa6bd57516ef9af254ec1
Yunsun Hwang <70623875+yunsun-hwang@users.noreply.github.com>
2022-02-22 23:55:40 -0600

    Project one draft

---
+++ draft/assets/js/site.js
@@ -0,0 +1,57 @@
+function formatPhoneNumber(value) {
+    if (!value) return value;
+    const phoneNumber = value.replace(/[^\d]/g, '');
+    const phoneNumberLength = phoneNumber.length;
+    if (phoneNumberLength < 4) return phoneNumber;
+    if (phoneNumberLength < 7) {
+      return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
+    }
+    return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
+      3,
+      6
+    )}-${phoneNumber.slice(6, 9)}`;
+}
+
+function phoneNumberFormatter() {
+    const inputField = document.getElementById('phone');
+    const formattedInputValue = formatPhoneNumber(inputField.value);
+    inputField.value = formattedInputValue;
+ }
+
+function formats(ele,e){
+    if(ele.value.length<19){
+      ele.value= ele.value.replace(/\W/gi, '').replace(/(.{4})/g, '$1 ');
+      return true;
+    }else{
+      return false;
+    }
+  }
+  
+function numberValidation(e){
+    e.target.value = e.target.value.replace(/[^\d ]/g,'');
+    return false;
+}
+
+function formatString(e) {
+    var inputChar = String.fromCharCode(event.keyCode);
+    var code = event.keyCode;
+    var allowedKeys = [8];
+    if (allowedKeys.indexOf(code) !== -1) {
+      return;
+    } 
+    event.target.value = event.target.value.replace(
+      /^([1-9]\/|[2-9])$/g, '0$1/' // 3 > 03/
+    ).replace(
+      /^(0[1-9]|1[0-2])$/g, '$1/' // 11 > 11/
+    ).replace(
+      /^([0-1])([3-9])$/g, '0$1/$2' // 13 > 01/3
+    ).replace(
+      /^(0?[1-9]|1[0-2])([0-9]{2})$/g, '$1/$2' // 141 > 01/41
+    ).replace(
+      /^([0]+)\/|[0]+$/g, '0' // 0/ > 0 and 00 > 0
+    ).replace(
+      /[^\d\/]|^[\/]*$/g, '' // To allow only digits and `/`
+    ).replace(
+      /\/\//g, '/' // Prevent entering more than 1 `/`
+    );
+}
